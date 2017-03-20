1ee1911f3765d06d1649b81ec61326499bf2775c
Aakil Fernandes <aakilfernandes@gmail.com>
2017-03-20 14:20:42 -0400

    just print version, not whole package.json

--- print.js
+++ print.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 
 commander
   .option('-n, --number <n>', 'number of twofa codes to print', parseInt)
-  .option('-f, --form <form>', 'output form (number/hex/hex.prefixed)', /^(number|hex|hex.prefixed)$/i)
   .parse(process.argv)
 
 const number = commander.number ? parseInt(commander.number) : 100
@@ -13,8 +12,9 @@ const form = commander.form ? commander.form : 'hex'
 const twofas = Array(number).fill(0).map(() => { return Twofa.generate() })
 const leftPadding = Array(digits).fill(' ').join('')
 const packageJson = fs.readFileSync('./package.json')
+const version = JSON.parse(packageJson).version
 
-console.log(JSON.stringify(JSON.parse(packageJson), null, 4))
+console.log(`eth-twofa@${version}:`)
 
 twofas.forEach((twofa, index) => {
   let indexString = index.toString()
